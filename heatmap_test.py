7063dc635141032fff0d7de2220660b260cef47c
SH\qkfzl <qkfzlfl99@gmail.com>
2023-12-05 18:19:52 +0900

    히트맵 어노테이터

--- heatmap_test.py
+++ heatmap_test.py
@@ -1,28 +1,50 @@
-import cv2
-import numpy as np
-from utils.heatmap_generator import HeatmapGenerator
+# import cv2
+# import numpy as np
+# from utils.heatmap_generator import HeatmapGenerator
+#
+# # 사용 예시
+# heatmap_generator = HeatmapGenerator(width=1280, height=853)
+#
+# # 50개의 랜덤 (x, y) 좌표에 랜덤한 값을 누적
+# # 실제 사용에서는 객체 검출 시의 x,y 값을 통해 범위를 정해서 누적
+# num_points = 50
+# for _ in range(num_points):
+#     random_x = np.random.randint(0, 1280)
+#     random_y = np.random.randint(0, 853)
+#     random_value = np.random.rand() * 15  # 0에서 10 사이의 랜덤한 값
+#     heatmap_generator.add_heat(x=random_x, y=random_y, value=random_value)
+#
+# heatmap_image = heatmap_generator.generate_heatmap()
+#
+# # 새로운 배경 이미지 불러오기
+# background_image = cv2.imread("assets/people.jpg")
+#
+# # 히트맵 이미지와 배경 이미지 합치기
+# heatmap_with_background = cv2.addWeighted(heatmap_image, 0.7, background_image, 0.3, 0)
+#
+# # 히트맵 이미지를 화면에 표시
+# cv2.imshow("Heatmap with Background", heatmap_with_background)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
-# 사용 예시
-heatmap_generator = HeatmapGenerator(width=1280, height=853)
+from supervision import get_video_frames_generator
 
-# 50개의 랜덤 (x, y) 좌표에 랜덤한 값을 누적
-# 실제 사용에서는 객체 검출 시의 x,y 값을 통해 범위를 정해서 누적
-num_points = 50
-for _ in range(num_points):
-    random_x = np.random.randint(0, 1280)
-    random_y = np.random.randint(0, 853)
-    random_value = np.random.rand() * 15  # 0에서 10 사이의 랜덤한 값
-    heatmap_generator.add_heat(x=random_x, y=random_y, value=random_value)
+import supervision as sv
+from ultralytics import YOLO
 
-heatmap_image = heatmap_generator.generate_heatmap()
+model = YOLO('yolov8n.pt')
 
-# 새로운 배경 이미지 불러오기
-background_image = cv2.imread("assets/people.jpg")
+heatmap_annotator = sv.HeatMapAnnotator()
 
-# 히트맵 이미지와 배경 이미지 합치기
-heatmap_with_background = cv2.addWeighted(heatmap_image, 0.7, background_image, 0.3, 0)
-
-# 히트맵 이미지를 화면에 표시
-cv2.imshow("Heatmap with Background", heatmap_with_background)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+video_info = sv.VideoInfo.from_video_path(video_path='assets/example_2.mp4')
+frames_generator = get_video_frames_generator(source_path='assets/exmaple_2.mp4')
+with sv.VideoSink(target_path='result_heatmap_example_2.mp4', video_info=video_info) as sink:
+    for frame in frames_generator:
+        result = model(frame)[0]
+        detections = sv.Detections.from_ultralytics(result)
+        detections = detections[detections.class_id == 0]
+        annotated_frame = heatmatp_annotator.annotate(
+            scene=frame.copy(),
+            detections=detections
+        )
+        sink.write_frame(frame=annotated_frame)
--- utils/heatmap_generator.py
+++ utils/heatmap_generator.py
@@ -1,51 +1,51 @@
-import cv2
-import numpy as np
-
-
-class HeatmapGenerator:
-    def __init__(self, width, height):
-        self.width = width
-        self.height = height
-        self.heatmap = np.zeros((height, width), dtype=np.float32)
-
-    def reset_heatmap(self):
-        """
-        누적된 값 초기화
-        """
-        self.heatmap = np.zeros((self.height, self.width), dtype=np.float32)
-
-    def add_heat(self, x, y, value=1):
-        """
-        x,y 좌표 값 누적
-
-        Args:
-            x (int): x 좌표
-            y (int): y 좌표
-            value (float): 누적할 값 (기본값: 1)
-        """
-        if 0 <= x < self.width and 0 <= y < self.height:
-            self.heatmap[y, x] += value
-
-    def generate_heatmap(self):
-        # 빈도수의 범위를 확인하여 가장 큰 값으로 나누어 강도를 조절
-        max_value = np.max(self.heatmap)
-        if max_value == 0:
-            max_value = 1  # 0으로 나누는 것을 방지
-
-        # 빈도수를 색상 강도로 변환하여 히트맵 생성
-        heatmap_image = cv2.applyColorMap(
-            np.uint8(255 * self.heatmap / max_value), cv2.COLORMAP_HOT
-        )
-
-        # 각 좌표에 대해 큰 크기로 점을 그림
-        for y, row in enumerate(self.heatmap):
-            for x, value in enumerate(row):
-                if value > 0:
-                    # 점 크기를 값에 비례하도록 설정 (원하는 크기로 조절 가능)
-                    radius = int(value)
-                    # 히트맵 이미지에 점을 그릴 때 빈도수를 고려하여 색상을 지정
-                    color_intensity = int(255 * value / max_value)
-                    color = (0, 0, color_intensity)  # 빨간색 계열로 설정
-                    cv2.circle(heatmap_image, (x, y), radius, color, thickness=-1)  # thickness=-1은 원을 채워 그림
-
-        return heatmap_image
+# import cv2
+# import numpy as np
+#
+#
+# class HeatmapGenerator:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#         self.heatmap = np.zeros((height, width), dtype=np.float32)
+#
+#     def reset_heatmap(self):
+#         """
+#         누적된 값 초기화
+#         """
+#         self.heatmap = np.zeros((self.height, self.width), dtype=np.float32)
+#
+#     def add_heat(self, x, y, value=1):
+#         """
+#         x,y 좌표 값 누적
+#
+#         Args:
+#             x (int): x 좌표
+#             y (int): y 좌표
+#             value (float): 누적할 값 (기본값: 1)
+#         """
+#         if 0 <= x < self.width and 0 <= y < self.height:
+#             self.heatmap[y, x] += value
+#
+#     def generate_heatmap(self):
+#         # 빈도수의 범위를 확인하여 가장 큰 값으로 나누어 강도를 조절
+#         max_value = np.max(self.heatmap)
+#         if max_value == 0:
+#             max_value = 1  # 0으로 나누는 것을 방지
+#
+#         # 빈도수를 색상 강도로 변환하여 히트맵 생성
+#         heatmap_image = cv2.applyColorMap(
+#             np.uint8(255 * self.heatmap / max_value), cv2.COLORMAP_HOT
+#         )
+#
+#         # 각 좌표에 대해 큰 크기로 점을 그림
+#         for y, row in enumerate(self.heatmap):
+#             for x, value in enumerate(row):
+#                 if value > 0:
+#                     # 점 크기를 값에 비례하도록 설정 (원하는 크기로 조절 가능)
+#                     radius = int(value)
+#                     # 히트맵 이미지에 점을 그릴 때 빈도수를 고려하여 색상을 지정
+#                     color_intensity = int(255 * value / max_value)
+#                     color = (0, 0, color_intensity)  # 빨간색 계열로 설정
+#                     cv2.circle(heatmap_image, (x, y), radius, color, thickness=-1)  # thickness=-1은 원을 채워 그림
+#
+#         return heatmap_image
